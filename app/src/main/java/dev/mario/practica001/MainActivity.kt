e4de0730099ba977713119dc17f2ecd90498451d
MarioMg24 <alexmendezgomez44@gmail.com>
2024-12-19 11:15:38 -0500

    primer envio cambio 1

--- app/src/main/java/dev/mario/practica001/MainActivity.kt
+++ app/src/main/java/dev/mario/practica001/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.mario.practica001.ui.theme.Practica001Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Practica001Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Saludo()
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Saludo(){
+    Column(
+        modifier = Modifier.padding(vertical = 50.dp, horizontal = 16.dp)
+    ) {
+        Text(
+            text = "hola",
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica001Theme {
-        Greeting("Android")
+        )
+        Text(
+            text = "Mi nombre es Goku",
+
+        )
     }
 }
